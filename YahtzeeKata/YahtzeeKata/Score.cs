addef200de121686920d0329fc3a889c07d3c863
Alan Campbell <alan.vine@outlook.com>
2019-05-04 09:41:16 +0100

    Iteration 1

---
+++ YahtzeeKata/YahtzeeKata/Score.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahtzeeKata
+{
+
+    public enum ScoreCategory
+    {
+        Units = 1,
+        Pair = 2,
+        TwoPairs = 3,
+        ThreeOfAKind = 4,
+        FourOfAKind = 5,
+        SmallStraight = 6,
+        LargeStraight = 7,
+        FullHouse = 8,
+        Yahtzee = 9,
+        Chance = 10
+    }
+
+    public class Game
+    {
+        public int GetScore(int[] dice, ScoreCategory category, 
+            int scoringDice = 0)
+        {
+            var result = 0;
+            List<int> singles = new List<int>();
+            List<int> pairs = new List<int>();
+            int threeOfAKind = 0;
+            int fourOfAKind = 0;
+            bool yahtzee = false;
+            bool smallStraight = true;
+            bool largeStraight = true;
+
+
+            singles.AddRange(dice.Where(a => a == scoringDice));
+            for(int x = 1; x <= 6; x++)
+            {
+                var matchingDice = dice.Where(a => a == x);
+                if (matchingDice.Count() >= 2)
+                {
+                    pairs.Add(x);
+                }
+                if (matchingDice.Count() >= 3)
+                {
+                    threeOfAKind = x;
+                }
+                if (matchingDice.Count() >= 4)
+                {
+                    fourOfAKind = x;
+                }
+                smallStraight = smallStraight 
+                    && (dice.Contains(x) || x == 6);
+
+                largeStraight = largeStraight
+                    && (dice.Contains(x) || x == 1);
+            }
+            yahtzee = dice.Distinct().Count() == 1;
+
+            return category == ScoreCategory.Units ? singlesScore(singles, scoringDice) :
+                category == ScoreCategory.Pair ? pairsScore(pairs) :
+                category == ScoreCategory.TwoPairs ? twoPairsScore(pairs) :
+                category == ScoreCategory.ThreeOfAKind ? threeOfAKindScore(threeOfAKind) :
+                category == ScoreCategory.FourOfAKind ? fourOfAKindScore(fourOfAKind) :
+                category == ScoreCategory.FullHouse ? fullHouseScore(pairs, threeOfAKind) :
+                category == ScoreCategory.SmallStraight ? smallStraight ? 15 : 0 :
+                category == ScoreCategory.LargeStraight ? largeStraight ? 20 : 0 :
+                category == ScoreCategory.Yahtzee ? yahtzee ? 50 : 0 : 
+                category == ScoreCategory.Chance ? chance(dice) : 0;
+        }
+
+        private int singlesScore(List<int> singles, int scoringDice)
+        {
+            return singles.Count() * scoringDice;
+        }
+
+        private int pairsScore(List<int> pairs)
+        {
+            return pairs?.LastOrDefault() * 2 ?? 0;
+        }
+
+        private int twoPairsScore(List<int> pairs)
+        {
+            return pairs.Count < 2 ? 0 :
+                (pairs[0] * 2) + (pairs[1] * 2);
+        }
+
+        private int threeOfAKindScore(int score)
+        {
+            return score * 3;
+        }
+
+        private int fourOfAKindScore(int score)
+        {
+            return score * 4;
+        }
+
+        private int fullHouseScore(List<int> pairs, int threeOfAKind)
+        {
+            var result = 0;
+            if(threeOfAKind > 0)
+            {
+                if(pairs.Any(a => a != threeOfAKind))
+                {
+                    result = (pairs.First(a => a != threeOfAKind) * 2)
+                        + (threeOfAKind * 3);
+                }
+            }
+            return result;
+        }
+
+        private int chance(int[] dice)
+        {
+            return dice.Sum(a => a);
+        }
+
+    }
+}
---
+++ YahtzeeKata/YahtzeeKata/YahtzeeTests.cs
@@ -0,0 +1,242 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahtzeeKata
+{
+    [TestFixture]
+    public class YahtzeeTests
+    {
+        Game _game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new Game();
+        }
+
+        [Test]
+        public void ThreeOnesReturnsCorrectResult()
+        {
+            Assert.AreEqual(3, 
+                _game.GetScore(new int[5]
+                {
+                    1, 1, 1, 3, 5
+                }, ScoreCategory.Units, 1));
+        }
+
+        [Test]
+        public void FourOnesReturnsCorrectResult()
+        {
+            Assert.AreEqual(4,
+                _game.GetScore(new int[5]
+                {
+                    1, 1, 1, 1, 5
+                }, ScoreCategory.Units, 1));
+        }
+
+        [Test]
+        public void ThreeTwosReturnsCorrectResult()
+        {
+            Assert.AreEqual(6,
+                _game.GetScore(new int[5]
+                {
+                    2, 2, 2, 1, 5
+                }, ScoreCategory.Units, 2));
+        }
+
+        [Test]
+        public void FourTwosReturnsCorrectResult()
+        {
+            Assert.AreEqual(8,
+                _game.GetScore(new int[5]
+                {
+                    2, 2, 2, 2, 5
+                }, ScoreCategory.Units, 2));
+        }
+
+        [Test]
+        public void FourThreessReturnsCorrectResult()
+        {
+            Assert.AreEqual(12,
+                _game.GetScore(new int[5]
+                {
+                    3, 3, 3, 3, 5
+                }, ScoreCategory.Units, 3));
+        }
+
+        [Test]
+        public void PairOfThreessReturnsCorrectResult()
+        {
+            Assert.AreEqual(6,
+                _game.GetScore(new int[5]
+                {
+                    3, 3, 1, 2, 5
+                }, ScoreCategory.Pair));
+        }
+
+        [Test]
+        public void PairReturnsMaximumPair()
+        {
+            Assert.AreEqual(10,
+                _game.GetScore(new int[5]
+                {
+                    3, 3, 5, 5, 6
+                }, ScoreCategory.Pair));
+        }
+
+        [Test]
+        public void PairReturnsMaximumPairWhenThereAreThree()
+        {
+            Assert.AreEqual(8,
+                _game.GetScore(new int[5]
+                {
+                    2, 2, 4, 4, 4
+                }, ScoreCategory.Pair));
+        }
+
+        [Test]
+        public void TowPairsReturnsCorrectScore()
+        {
+            Assert.AreEqual(16,
+                _game.GetScore(new int[5]
+                {
+                    3, 3, 5, 5, 6
+                }, ScoreCategory.TwoPairs));
+        }
+
+        [Test]
+        public void ThreeOfAKindReturnsCorrectScore()
+        {
+            Assert.AreEqual(15,
+                _game.GetScore(new int[5]
+                {
+                    3, 3, 5, 5, 5
+                }, ScoreCategory.ThreeOfAKind));
+        }
+
+        [Test]
+        public void ThreeOfAKindWhenThereAreFourReturnsCorrectScore()
+        {
+            Assert.AreEqual(15,
+                _game.GetScore(new int[5]
+                {
+                    3, 5, 5, 5, 5
+                }, ScoreCategory.ThreeOfAKind));
+        }
+
+        [Test]
+        public void FourOfAKindReturnsCorrectScore()
+        {
+            Assert.AreEqual(20,
+                _game.GetScore(new int[5]
+                {
+                    3, 5, 5, 5, 5
+                }, ScoreCategory.FourOfAKind));
+        }
+
+        [Test]
+        public void FourOfAKindWhenThereAreFiveReturnsCorrectScore()
+        {
+            Assert.AreEqual(20,
+                _game.GetScore(new int[5]
+                {
+                    5, 5, 5, 5, 5
+                }, ScoreCategory.FourOfAKind));
+        }
+
+        [Test]
+        public void FullHouseReturnsCorrectScore()
+        {
+            Assert.AreEqual(21,
+                _game.GetScore(new int[5]
+                {
+                    5, 5, 5, 3, 3
+                }, ScoreCategory.FullHouse));
+        }
+
+        [Test]
+        public void NotFullHouseReturnsCorrectScore()
+        {
+            Assert.AreEqual(0,
+                _game.GetScore(new int[5]
+                {
+                    5, 5, 1, 3, 3
+                }, ScoreCategory.FullHouse));
+        }
+
+        [Test]
+        public void YahtzeeReturnsCorrectScore()
+        {
+            Assert.AreEqual(50,
+                _game.GetScore(new int[5]
+                {
+                    5, 5, 5, 5, 5
+                }, ScoreCategory.Yahtzee));
+        }
+
+        [Test]
+        public void FailedYahtzeeReturnsCorrectScore()
+        {
+            Assert.AreEqual(0,
+                _game.GetScore(new int[5]
+                {
+                    5, 5, 2, 5, 5
+                }, ScoreCategory.Yahtzee));
+        }
+
+        [Test]
+        public void SmallStraightReturnsCorrectScore()
+        {
+            Assert.AreEqual(15,
+                _game.GetScore(new int[5]
+                {
+                    1, 3, 2, 5, 4
+                }, ScoreCategory.SmallStraight));
+        }
+
+        [Test]
+        public void FailedSmallStraightReturnsCorrectScore()
+        {
+            Assert.AreEqual(0,
+                _game.GetScore(new int[5]
+                {
+                    1, 3, 2, 6, 4
+                }, ScoreCategory.SmallStraight));
+        }
+
+        [Test]
+        public void LargeStraightReturnsCorrectScore()
+        {
+            Assert.AreEqual(20,
+                _game.GetScore(new int[5]
+                {
+                    6, 3, 2, 5, 4
+                }, ScoreCategory.LargeStraight));
+        }
+
+        [Test]
+        public void FailedLargeStraightReturnsCorrectScore()
+        {
+            Assert.AreEqual(0,
+                _game.GetScore(new int[5]
+                {
+                    6, 3, 1, 5, 4
+                }, ScoreCategory.LargeStraight));
+        }
+
+        [Test]
+        public void ChanceReturnsCorrectScore()
+        {
+            Assert.AreEqual(19,
+                _game.GetScore(new int[5]
+                {
+                    6, 3, 1, 5, 4
+                }, ScoreCategory.Chance));
+        }
+
+    }
+}
